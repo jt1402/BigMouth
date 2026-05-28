00bc75c066d9d9f8895bc696ac3802e19440e48f
Jay <jturgunov1402@gmail.com>
2026-05-28 18:38:16 +0900

    Default locale: Korean

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "ko"],
-  defaultLocale: "en",
+  defaultLocale: "ko",
   localePrefix: "as-needed",
 });
 
